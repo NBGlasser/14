92b8333c3ae34677818ce3d78e6748c4fb7d4002
Noah G <NoahBGlasser@gmail.com>
2019-04-19 22:17:58 -0700

    bare functionality

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 var express = require("express");
-var mongojs = require("mongojs");
+// var mongojs = require("mongojs");
+var logger = require("morgan");
 var axios = require("axios");
 var cheerio = require("cheerio");
+var mongoose = require("mongoose");
+
+
+var db = require("./models");
+
+var PORT = 3000;
 
 var app = express();
+app.use(logger("dev"));
+
 
 databaseURL = "articledb"
 collections = ["articles"]
 
-var db = mongojs(databaseURL, collections)
-db.on("error", function (error) {
-    console.log("Database Error:", error);
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/14-news", { useNewUrlParser: true });
+
+
+// var db = mongojs(databaseURL, collections)
+// db.on("error", function (error) {
+//     console.log("Database Error:", error);
+// });
 
 app.get("/", function (req, res) {
     res.render("index.html");
@@ -23,11 +39,61 @@ app.get("/scrape", function (req, res) {
         $(".content-wrapper").each(function (i, element) {
             var result = {}
             result.title = $(this).find(".content-meta__headline__wrapper").children().children().text()
+            result.link = $(this).find(".content-meta__headline__wrapper").children().children("a").attr("href")
             console.log(result)
 
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+
         })
+        res.send("Scrape Complete");
     })
 });
+
+app.get("/articles", function(req, res) {
+    db.Article.find({}).then(function(data){
+      res.json(data)
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+  });
+  
+  app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({_id: req.params.id})
+    .populate("note")
+    .then(function(data){
+      console.log(data)
+      res.json(data)
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+  });
+  
+  app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body).then(function(note){
+      console.log(req.params.id)
+      return db.Article.findOneAndUpdate({
+        query: {_id: req.params.id},
+        update: { $set: { note: {
+          title: note.title,
+          body: note.body
+        } } }
+        
+      }).then(function(data){
+        console.log(data)
+        res.json(data)
+      })
+      
+  
+    })
+  })
 
 app.listen(3000, function () {
     console.log("App running on port 3000!");
