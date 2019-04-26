a0767ab5d1bafef128d2da7cd4fc99394ce4f105
Noah G <NoahBGlasser@gmail.com>
2019-04-26 11:18:48 -0700

    Change getJSON to standard get method.

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-$.getJSON("/articles", function (data) {
+$.get("/articles", function (data) {
     for (var i = 0; i < data.length; i++) {
         $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br /></p>");
         $("#articles").append("<a href=" + data[i].link + ">Link</a>" )
